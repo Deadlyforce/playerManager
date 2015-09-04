1363f23cdd362773969c3d08f9cb40d54228251d
Norman <norman.rosenstech@free.fr>
2015-09-04 22:33:56 +0200

    commit du 04-09-15 22h33

---
+++ src/AppBundle/Entity/Source.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Source
+ *
+ * @ORM\Table(name="sources")
+ * @ORM\Entity(repositoryClass="AppBundle\Entity\SourceRepository")
+ */
+class Source
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="libelle", type="string", length=255, nullable=false)
+     */
+    private $libelle;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set libelle
+     *
+     * @param string $libelle
+     *
+     * @return Source
+     */
+    public function setLibelle($libelle)
+    {
+        $this->libelle = $libelle;
+        return $this;
+    }
+
+    /**
+     * Get libelle
+     *
+     * @return string
+     */
+    public function getLibelle()
+    {
+        return $this->libelle;
+    }
+    
+    /**
+     * Retourne le libellé de l'entité (son nom)
+     *  
+     * @return string
+     */
+    public function __toString() 
+    {
+        return $this->getLibelle();
+    }
+}
+
