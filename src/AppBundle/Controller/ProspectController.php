40b1757646a08b853107f297842cbeda5eb9df8f
Deadlyforce <norman.rosenstech@free.fr>
2016-03-27 22:04:48 +0200

    Moved prospect views to Frontend folder

--- src/AppBundle/Controller/ProspectController.php
+++ src/AppBundle/Controller/ProspectController.php
@@ -41,7 +41,7 @@ class ProspectController extends Controller
         $prospect->setDateCreation($datetime);
         
         $form = $this->createCreateForm($prospect)->createView();
-        $form_view = $this->renderView("AppBundle:Prospect:new.html.twig", array('form' => $form));
+        $form_view = $this->renderView("AppBundle:Fronted/Prospect:new.html.twig", array('form' => $form));
                 
         return new Response($form_view);
     }
@@ -95,7 +95,7 @@ class ProspectController extends Controller
      *
      * @Route("/", name="prospect")
      * @Method("GET")
-     * @Template()
+     * @Template("AppBundle:Frontend/Prospect:index.html.twig")
      */
     public function indexAction()
     {
@@ -125,7 +125,7 @@ class ProspectController extends Controller
      *
      * @Route("/", name="prospect_create")
      * @Method("POST")
-     * @Template("AppBundle:Prospect:new.html.twig")
+     * @Template("AppBundle:Frontend/Prospect:new.html.twig")
      */
     public function createAction(Request $request)
     {
@@ -195,7 +195,7 @@ class ProspectController extends Controller
      *
      * @Route("/new", name="prospect_new")
      * @Method("GET")
-     * @Template()
+     * @Template("AppBundle:Frontend/Prospect:new.html.twig")
      */
     public function newAction()
     {
@@ -224,7 +224,7 @@ class ProspectController extends Controller
      *
      * @Route("/{id}", name="prospect_show")
      * @Method("GET")
-     * @Template()
+     * @Template("AppBundle:Frontend/Prospect:show.html.twig")
      */
     public function showAction($id)
     {
@@ -258,7 +258,7 @@ class ProspectController extends Controller
      *
      * @Route("/{id}/edit", name="prospect_edit")
      * @Method("GET")
-     * @Template()
+     * @Template("AppBundle:Frontend/Prospect:edit.html.twig")
      */
     public function editAction($id)
     {
@@ -311,7 +311,7 @@ class ProspectController extends Controller
      *
      * @Route("/{id}", name="prospect_update")
      * @Method("PUT")
-     * @Template("AppBundle:Prospect:edit.html.twig")
+     * @Template("AppBundle:Frontend/Prospect:edit.html.twig")
      */
     public function updateAction(Request $request, $id)
     {
